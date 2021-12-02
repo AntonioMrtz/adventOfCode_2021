6e18ff3cd031799271bfdc87641412a728922773
AntonioUbuntu <antoniomartinezf@um.es>
2021-12-02 11:03:25 +0100

    02 - part1

--- 02.py
+++ 02.py
@@ -5,9 +5,56 @@ Created on 01 december 2021
 '''
 
 
-def main_v1():
-    pass
+def lectura():
 
+    archivo=open("input02.txt")
+    # archivo=open("prueba.txt")
+    return archivo
+
+def main_v1():
+
+    archivo=lectura()
+    
+    x=0     #horizontal
+    y=0     #vertical
+
+
+    while True:
+
+        linea=archivo.readline()
+
+        if not linea:
+            break
+
+        #implementacion
+        
+        steps=int(linea.split()[1])
+
+        if linea.startswith("f"):
+            #forward
+
+            x+=steps
+
+        elif linea.startswith("u"):
+            #up
+
+            y-=steps
+
+
+        elif linea.startswith("d"):
+            #down
+
+            y+=steps
+
+
+
+    return x*y
+
+
+
+def main_v2():
+    pass
+    
 if __name__ == '__main__':
     print(f" part 1: {main_v1()}")
 
