494da50dd5fc4319051b7e3b1e90d96369f2c354
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2021-12-06 19:27:01 +0100

    05 - initialize

---
+++ 05.py
@@ -0,0 +1,14 @@
+'''
+Created on ____ december 2021
+
+@author: Antonio Martínez Fernández
+'''
+
+def main_v1():
+    pass
+
+def main_v2():
+    pass
+
+if __name__ == '__main__':
+    print(f"part 1 : {main_v1()} \npart 2 : {main_v2()}")
