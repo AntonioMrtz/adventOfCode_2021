e0740c0dc20de3983767c48e425e9e199b5845f6
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2022-02-12 21:44:45 +0100

    05 - part 1 list

--- 05.js
+++ 05.js
@@ -1,9 +1,65 @@
 const { start } = require("repl");
 
+
+class Coordenates {
+
+    constructor(x, y) {
+
+        this.x = x;
+        this.y = y;
+        this.value = 1;
+
+
+    }
+
+    add_value() {
+
+        this.value += 1;
+
+    }
+
+
+}
+
+function checkIfIn(coordenate) {
+
+    for (let i = 0; i < entries.length; i++) {
+
+        if (entries[i].x == coordenate.x && entries[i].y == coordenate.y) {
+
+            return entries[i];
+
+        }
+
+
+    }
+
+    return false;
+
+}
+
+
+function addValue(coordenate) {
+
+    for (let i = 0; i < entries.length; i++) {
+
+        if (entries[i].x == coordenate.x && entries[i].y == coordenate.y) {
+
+            entries[i].add_value();
+        }
+
+
+    }
+
+
+}
+
+
+
 function readFile() {
     let fs = require("fs");
     //let array_aux = fs.readFileSync("input05.txt").toString().split("\n");
-    let array_aux = fs.readFileSync("prueba.txt").toString().split("\n");
+    let array_aux = fs.readFileSync("input05.txt").toString().split("\n");
 
 
     for (i in array_aux) {
@@ -12,43 +68,55 @@ function readFile() {
 
 }
 
-function getEntries(line){
+function getEntries(line) {
 
-    line=line.trim();
-    line=line.split(" -> ");
+    line = line.trim();
+    line = line.split(" -> ");
 
-    let start=line[0];
-    let end=line[1];
+    let start = line[0];
+    let end = line[1];
 
-    start=start.split(",");
-    end=end.split(",");
+    start = start.split(",");
+    end = end.split(",");
 
 
-    let start_x=start[0];
-    let start_y=start[1];
-    let end_x=end[0];
-    let end_y=end[1];
+    let start_x = start[0];
+    let start_y = start[1];
+    let end_x = end[0];
+    let end_y = end[1];
 
-    fillEntries(parseInt(start_x),parseInt(start_y),parseInt(end_x),parseInt(end_y));
+    fillEntries(parseInt(start_x), parseInt(start_y), parseInt(end_x), parseInt(end_y));
 
 
 }
 
-function fillEntries(start_x,start_y,end_x,end_y){
+function fillEntries(start_x, start_y, end_x, end_y) {
 
     // si nos encontramos un 1 ponemos 2
     // si encontramos 2 dejamos igual
 
-    console.log(start_x,start_y,end_x,end_y);
+    //console.log(start_x, start_y, end_x, end_y);
 
-    if(start_x==end_x){
+    if (start_x == end_x) {
 
-        if(start_y>end_y){
+        if (start_y > end_y) {
 
-            for(let i=end_y;i<start_y;i++){
+            for (let i = end_y; i <= start_y; i++) {
 
-                if(entries.get())
-                entries.set(start_x,i);
+
+                let obj=checkIfIn(new Coordenates(start_x, i));
+
+                if (obj!=false) {
+
+                    addValue(obj);
+
+                }
+
+                else {
+
+                    entries.push(new Coordenates(start_x,i));
+
+                }
 
             }
 
@@ -57,30 +125,53 @@ function fillEntries(start_x,start_y,end_x,end_y){
 
         else {
 
-            for(let i=start_y;i<end_y;i++){
+            for (let i = start_y; i <= end_y; i++) {
 
-                entries.set(start_x,i);
+
+                let obj=checkIfIn(new Coordenates(start_x, i));
+
+                if (obj!=false) {
+
+                    addValue(obj);
+
+                }
+
+                else {
+
+                    entries.push(new Coordenates(start_x,i));
+
+                }
 
             }
+
+
+
+
+            
 
 
         }
 
     }
 
-    else if(start_y==end_y){
+    else if (start_y == end_y) {
 
-        if(start_x>end_x){
+        if (start_x > end_x) {
 
-            for(let i=end_x;i<start_x;i++){
+            for (let i = end_x; i <= start_x; i++) {
 
-                console.log(entries[i]);
-                entries[i]+=1;
+                let obj=checkIfIn(new Coordenates(i, start_y));
 
+                if (obj!=false) {
 
-                if(entries[i]==null){
+                    addValue(obj);
 
-                    console.log("ifwjuiwjwf");
+                }
+
+                else {
+
+                    entries.push(new Coordenates(i,start_y));
+
                 }
 
             }
@@ -91,10 +182,25 @@ function fillEntries(start_x,start_y,end_x,end_y){
         else {
 
 
-            for(let i=start_x;i<end_x;i++){
+            for (let i = start_x; i <= end_x; i++) {
 
-                console.log(entries[i]);
-                entries[i]+=1;
+               
+
+                    let obj=checkIfIn(new Coordenates(i, start_y));
+    
+                    if (obj!=false) {
+    
+                        addValue(obj);
+    
+                    }
+    
+                    else {
+    
+                        entries.push(new Coordenates(i,start_y));
+    
+                    }
+    
+                
 
 
             }
@@ -109,34 +215,46 @@ function fillEntries(start_x,start_y,end_x,end_y){
 
 }
 
-function checkResult(){
+function checkResult() {
 
-    // del entries comprobar los numeros 2 que hay
 
-    let counter=0;
+    let counter = 0;
 
-    //TODO
+    for(let i=0;i<entries.length;i++){
+
+        if (entries[i].value >= 2){
+
+            counter += 1;
+
+        }
+
+    }
+
+
+    
 
     return counter;
 
 }
 
-var array=[];
-var entries=new Map();
 
 
-var result=0;
+var array = [];
+var entries = [];
+
+
+var result = 0;
 
 
 readFile();
 
-for (let line of array){
+for (let line of array) {
 
     getEntries(line);
-    //console.log(entries);
-
+    
 }
 
-//console.log(`Part 1: ${checkResult()} \n`);
+//console.log(entries);
+console.log(`Part 1: ${checkResult()} \n`);
 
 
