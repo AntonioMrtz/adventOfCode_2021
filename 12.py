44d6011775e3161ff40976d934f0e4f60c0f5bf1
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2022-06-30 21:38:19 +0200

    12 - part 2 DONE

--- 12.py
+++ 12.py
@@ -1,11 +1,14 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 
 
 routes = {}
 
 ways=0
+
+all_paths=[]
+
 
 def read_routes():
 
@@ -37,7 +40,6 @@ def read_routes():
 
                 routes[src].append(dst)
 
-        #if src != "end" and src!="start" and dst!="end"  :
         if  src!="start" and dst!="end"  :
 
 
@@ -52,14 +54,7 @@ def read_routes():
 
 
     logging.debug(routes)
-  
 
-
-
-
-# solo pilla
-
-# start,A,b,end | start,A,end | start,b,end
 
 def recursive_finding(src,visited):   
 
@@ -86,6 +81,7 @@ def recursive_finding(src,visited):
 
                 recursive_finding(dst, visited[:]) 
 
+
 def compute_paths():
 
     global ways
@@ -101,10 +97,86 @@ def main_p1():
 
     read_routes()
     return compute_paths()
+
+
+def get_small_caves():
+
+    small_caves=[]
+
+    for cave in routes:
+
+        if cave!="start" and cave!="end" and cave.islower():
+
+            small_caves.append(cave)
+
+
+    return small_caves
+
+
+def recursive_finding_repeated_cave(src,visited,small_cave,flag,path):   
+
+    global ways
+
+    path.append(src)
+
+    if src=="end" and path not in all_paths:
+        
+        all_paths.append(path)
+
+        #logging.debug("part2: "+path)
+        
+        ways +=1
+        return
+
+
+    if src in routes:
+
+        if src==small_cave and flag==1:
+
+            visited.append(src)
+            flag=2
+
+        elif src==small_cave and flag==0:
+
+            flag=1
+
+        elif src.islower():
+
+            visited.append(src)
+
+        for dst in routes[src]:
+
+            if dst not in visited:
+
+                recursive_finding_repeated_cave(dst, visited[:],small_cave,flag,path[:]) 
+
+
+def compute_paths_p2():
+
+    global ways
+
+    small_caves=get_small_caves()
+
+    for small_cave in small_caves:
+
+        for dst in routes["start"]:
+
+            recursive_finding_repeated_cave(dst,[],small_cave,0,[]) 
+
+    return ways
+
+
+def main_p2():
+
+    global ways
+
+    ways=0
+
+    return compute_paths_p2()
     
 
 if __name__ == "__main__":
   
     print(f"part 1 : {main_p1()}\n")
-    #print(f"part 2 : {main_p2()}\n")  
+    print(f"part 2 : {main_p2()}\n")  
   
